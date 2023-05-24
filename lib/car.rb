d396ccccc571a85efa9b41406fc76ffee2d4d6b7
KiruiTito <titokirui402@gmail.com>
2023-05-24 15:28:45 +0300

    Completed assignmnent

--- lib/car.rb
+++ lib/car.rb
@@ -1,19 +1,19 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 class Vehicle
 
-    attr_accessor :wheel_size, :wheel_number
-  
-    def initialize(wheel_size, wheel_number)
-      @wheel_size = wheel_size
-      @wheel_number = wheel_number
-    end
-  
-    def go
-      "vrrrrrrrooom!"
-    end
-  
-    def fill_up_tank
-      "filling up!"
-    end
-  
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
+  class Car < Vehicle
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+
+end
